9d027e0b4f39e681da063fba66732a5b7e8e1b1d
ADARSH THAKUR <adarsht6202@gmail.com>
2025-05-28 16:47:10 +0530

    First Commit

--- payment_app/frontend/src/component/Appbar.jsx
+++ payment_app/frontend/src/component/Appbar.jsx
@@ -1,7 +1,7 @@
 export const Appbar=({user})=>{
   return <div className="shadow h-14 flex justify-between">
     <div className="flex flex-col justify-center h-full ml-4">
-      PayTM App
+      Payment_App
     </div>
     <div className="flex">
       <div className="flex flex-col justify-center h-full mr-4">
